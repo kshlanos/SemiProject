37dc51151b70dc3a8dea480640744d0ef7446e27
hi <hi@DESKTOP-F4BC13L>
2022-09-26 18:23:36 +0900

    commit

---
+++ src/main/java/com/footfire/project/FootfireprojectApplication.java
@@ -0,0 +1,13 @@
+package com.footfire.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootfireprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootfireprojectApplication.class, args);
+	}
+
+}
